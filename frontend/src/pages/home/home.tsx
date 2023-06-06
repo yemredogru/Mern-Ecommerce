ef2c0e52b83af6974a27cbd3da943d801f541adb
yemredogru <yemredogrubp@gmail.com>
2023-06-06 09:44:17 +0300

    components and homepage created

---
+++ frontend/src/pages/home/home.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import Footer from "@/components/footers/footer";
+import Header from "@/components/headers/headers";
+
+
+export default function HomePage() {
+    return (
+        <div>
+            <Footer />
+            <Header />
+        </div>
+    )
+}
