0aa44ab3c7bbf52d319cc9c6e3aee8058ac457e1
yemredogru <yemredogrubp@gmail.com>
2023-06-07 11:23:44 +0300

    password hash usermodel

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const schema = mongoose.Schema({
     firstName:String,
     lastName:String,
@@ -14,8 +15,40 @@ const schema = mongoose.Schema({
         required: 'Email address is required',
         validate: [validateEmail, 'Please fill a valid email address'],
         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    bucket:{
+        type:Array
+    },
+    orders:{
+        type:Array
+    },
+    refresh_token:{
+        type:String,
+        unique:true
+    },
+    password:{
+        type:String
     }
     
 })
+UserSchema.pre('save', function(next) {
+    var user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
 const User = mongoose.model('User',schema);
 module.exports = User
