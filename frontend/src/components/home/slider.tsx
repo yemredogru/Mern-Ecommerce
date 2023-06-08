b007aaf551d3aede8ef6a31a3700acbc916ea64f
yemredogru <yemredogrubp@gmail.com>
2023-06-08 23:53:04 +0300

    homepage slider

---
+++ frontend/src/components/home/slider.tsx
@@ -0,0 +1,12 @@
+import React, {useState,useEffect, Component} from 'react';
+
+export default class Slider extends Component{
+    render(){
+        return(
+            <div className='col-lg-8'>
+                <img src='https://img.imgyukle.com/2023/06/08/raNBhS.png' />
+            </div>
+        )
+    }
+}
+ 
--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Footer from "@/components/footers/footer";
 import Header from "@/components/headers/headers";
-
+import Slider from "@/components/home/slider";
 
 export default function Home() {
     return (
         <div>
             <Header />
+            <Slider />
             <Footer />
         </div>
     )
