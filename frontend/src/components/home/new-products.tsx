99dd4fcdd9f54de27b39ba603fc51c4c23feb2c0
yemredogru <yemredogrubp@gmail.com>
2023-06-09 11:27:36 +0300

    new products page added and gitattributes created

---
+++ frontend/src/components/home/new-products.tsx
@@ -0,0 +1,16 @@
+import React, {useState,useEffect} from 'react';
+import './new-products.css'
+
+export default function NewProducts(){
+    return(
+        <div>
+            <div className='headers'>
+                <p>New Products</p>
+                <a>See All New Products</a>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from "react";
 import Footer from "@/components/footers/footer";
 import Header from "@/components/headers/headers";
 import Slider from "@/components/home/slider";
+import NewProducts from "@/components/home/new-products";
 
 export default function Home() {
     return (
         <div>
             <Header />
             <Slider />
+            <NewProducts />
             <Footer />
         </div>
     )
