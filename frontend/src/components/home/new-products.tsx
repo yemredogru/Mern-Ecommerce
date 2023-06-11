2de17a5769f34e9afaafa0c152b919f02c19fb97
yemredogru <yemredogrubp@gmail.com>
2023-06-11 11:00:49 +0300

    swiper added

--- frontend/src/components/home/new-products.tsx
+++ frontend/src/components/home/new-products.tsx
@@ -1,4 +1,7 @@
 import React, {useState,useEffect} from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
 import './new-products.css'
 
 export default function NewProducts(){
@@ -9,7 +12,68 @@ export default function NewProducts(){
                 <a>See All New Products</a>
             </div>
             <div>
-
+            <Swiper
+      spaceBetween={50}
+      slidesPerView={6}
+      loop={true}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>
+        <div>
+            <p>in stock</p>
+            <p>Reviews (4)</p>
+            <p>EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...</p>
+            <p>$499.00</p>
+            <p>$499.00</p>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div>
+            <p>in stock</p>
+            <p>Reviews (4)</p>
+            <p>EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...</p>
+            <p>$499.00</p>
+            <p>$499.00</p>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div>
+            <p>in stock</p>
+            <p>Reviews (4)</p>
+            <p>EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...</p>
+            <p>$499.00</p>
+            <p>$499.00</p>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div>
+            <p>in stock</p>
+            <p>Reviews (4)</p>
+            <p>EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...</p>
+            <p>$499.00</p>
+            <p>$499.00</p>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div>
+            <p>in stock</p>
+            <p>Reviews (4)</p>
+            <p>EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...</p>
+            <p>$499.00</p>
+            <p>$499.00</p>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div>
+            <p>in stock</p>
+            <p>Reviews (4)</p>
+            <p>EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...</p>
+            <p>$499.00</p>
+            <p>$499.00</p>
+        </div>
+      </SwiperSlide>
+    </Swiper>
             </div>
         </div>
     )
--- frontend/src/components/home/slider.tsx
+++ frontend/src/components/home/slider.tsx
@@ -4,7 +4,7 @@ export default class Slider extends Component{
     render(){
         return(
             <div className='col-lg-8'>
-                <img src='https://img.imgyukle.com/2023/06/08/raNBhS.png' />
+                <img src='https://img.imgyukle.com/2023/06/11/rH1Nhj.png' />
             </div>
         )
     }
